4a852bff7bac93c4fc6d4cc0e796e34562dcc1ff
David Knaack <david@knaack.me>
2015-05-05 18:48:04 +0200

    add missing :

--- shuffle.nim
+++ shuffle.nim
@@ -18,5 +18,5 @@ proc gaussRand*(länge: int): int =
     result = -1
     let tmp = 1 + länge.toFloat / PI * cos(2*PI*random(1.0)) * sqrt(-2*log10(random(1.0)))
     result = tmp.toInt
-    if result < 0
+    if result < 0:
       result = -result
