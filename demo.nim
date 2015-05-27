323b1650766c633e628ec23cbdd9a6f1c607e00c
David Knaack <david@knaack.me>
2015-05-27 08:51:14 +0200

    demo

--- demo.nim
+++ demo.nim
@@ -1,7 +1,10 @@
-from listSpawner import shuffledKeys
+import listSpawner
 import os
 import bench
 import sorts
+import parseopt2
+
+const max = 5_000
 
 let appDirPath = getAppDir()
 let testDataPath = appDirPath / "testData"
@@ -9,10 +12,29 @@ let testDataPath = appDirPath / "testData"
 if not existsDir testDataPath:
     createDir testDataPath
 
-const max = 5_000
+template demoIt(listSpawner: stmt) =
+    demoBenchMark(radixSort, listSpawner, "Radix LSD Sort", max, 1)
+    demoBenchMark(quickSort, listSpawner, "QuickSort", max, 1)
+    demoBenchMark(insertionSort, listSpawner, "Insertion Sort", max, 1)
 
-demoBenchMark(radixSort, shuffledKeys, "RS", max, 1)
+var param = ""
+if paramCount() != 0:
+    param = paramStr(1)
 
-demoBenchMark(quickSort, shuffledKeys, "QS", max, 1)
 
-demoBenchMark(insertionSort, shuffledKeys, "IS", max, 1)
+case param:
+    of "fewUnique":
+        demoIt fewUniqueKeys
+    of "almostSorted":
+        demoIt almostSortedKeys
+    of "almostReverse":
+        demoIt almostReverseKeys
+    of "random":
+        demoIt randKeys
+    of "normal":
+        demoIt normalKeys
+    else:
+        demoIt shuffledKeys
+
+
+
--- demo.py
+++ demo.py
@@ -2,28 +2,43 @@ import os
 import glob
 import locale
 import subprocess
+import sys
 import pandas as pd
 import seaborn as sns
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
+# META
 locale.setlocale(locale.LC_ALL, "de_DE")
 mpl.rcParams['axes.formatter.use_locale'] = True
 sns.set_context("paper")
 
+# Get app dir
 app_dir = os.path.dirname(os.path.realpath(__file__))
-print(app_dir)
-print("Benchmarking…")
-subprocess.call(app_dir + "/demo")
+os.chdir(app_dir)
 
+# Build (if needed)
+print("Building…")
+subprocess.call(["nim", "c", "-d:release", "demo"])
+
+# Benchmark
+print("Benchmarking…")
+arg = ""
+if len(sys.argv) > 1:
+	arg = sys.argv[1]
+subprocess.call([app_dir + "/demo", arg])
+
+
+# Plot
+print("Plotting…")
 fig = plt.figure()
 ax = fig.add_subplot(111)
 
-for i in [["IS", "Insertion Sort", "r"], ["QS", "QuickSort", "g"], ["RS", "RadixSort", "b"]]:
-		print("Plotting…")
+for i in [["Insertion Sort", "r"], ["QuickSort", "g"], ["Radix LSD Sort", "b"]]:
+		print("Plotting " + i[0] + " …")
 		data = pd.io.parsers.read_csv(
 			app_dir + "/testData/" + i[0] + ".csv", names=["x", "y"], header=None)
-		ax.scatter(data["x"], data["y"], s=1, c=i[2], lw=0, label=i[1])
+		ax.scatter(data["x"], data["y"], s=1, c=i[1], lw=0, label=i[0])
 
 ax.set_xlabel("Wiederholungen")
 ax.set_ylabel("Zeit in s")
